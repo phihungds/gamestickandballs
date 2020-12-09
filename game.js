c028b74bf795b7158f77ef6932eef9b32a3bc496
Phi Hung <phihunds1@gmail.com>
2020-12-09 20:58:34 +0700

    addstick

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 let ctx = document.getElementById("game").getContext("2d")
 let ball = new Ball(20, 20, 4, 4, 20, 20, ctx)
+let stick = new Stick(150, 540, 100, 10, ctx)
 timeLoop = 1000 / 30
 setInterval(gameStart, timeLoop)
 function gameStart() {
     ball.ctx.clearRect(0, 0, 800, 600)
     ball.move()
     ball.draw()
+    stick.draw()
 }
 
--- stick.js
+++ stick.js
@@ -4,4 +4,11 @@ function Stick(x, y, w, h, ctx) {
     this.w = w
     this.h = h 
     this.ctx = ctx
+    
+    
+    this.draw = function () {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#000000';
+        this.ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
 }
